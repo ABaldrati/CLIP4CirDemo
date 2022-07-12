b39fc068a29644015db95bda9b96e7889b79513c
Alberto Baldrati <a_baldrati@hotmail.it>
2022-07-12 18:23:26 +0200

    feat(*): add the possibility of uploading the reference image

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import json
+import os
 import pickle
 import random
+import time
+from io import BytesIO
+from multiprocessing import Process
+from typing import Optional, Tuple, Union
+
 import PIL.Image
 import PIL.ImageOps
 import clip
@@ -8,12 +14,14 @@ import numpy as np
 import torch
 from flask import Flask, send_file, url_for
 from flask import render_template, request, redirect
-from io import BytesIO
-from typing import Optional, Tuple, Union
-from data_utils import targetpad_resize, server_base_path, data_path
+from torchvision.transforms.functional import resize
+from werkzeug.utils import secure_filename
+
+from data_utils import targetpad_resize, targetpad_transform, server_base_path, data_path
 from model import Combiner
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = server_base_path / 'uploaded_files'
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
@@ -34,6 +42,42 @@ def choice():
 @app.route('/favicon.ico')
 def favicon():
     return url_for('static', filename='/favicon.ico')
+
+
+@app.route('/file_upload/<string:dataset>', methods=['POST'])
+def file_upload(dataset: str):
+    """
+    Upload a reference image not included in the datasets
+    :param dataset: dataset where upload the reference image
+    """
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(url_for('choice'))
+        file = request.files['file']
+        if file:
+            try:
+                img = PIL.Image.open(file)
+            except Exception:  # If the file is not an image redirect to the reference choice page
+                return redirect(url_for('reference', dataset=dataset))
+            resized_img = resize(img, 512, PIL.Image.BICUBIC)
+
+            filename = secure_filename(file.filename)
+            filename = os.path.splitext(filename)[0] + str(int(time.time())) + os.path.splitext(filename)[
+                1]  # Append the timestamp to avoid conflicts in names
+            if 'fiq-category' in request.form:  # if the user upload an image to FashionIQ it must specify the category
+                assert dataset == 'fashionIQ'
+                fiq_category = request.form['fiq-category']
+                folder_path = app.config['UPLOAD_FOLDER'] / dataset / fiq_category
+                folder_path.mkdir(exist_ok=True, parents=True)
+                resized_img.save(folder_path / filename)
+            else:
+                assert dataset == 'cirr'
+                folder_path = app.config['UPLOAD_FOLDER'] / dataset
+                folder_path.mkdir(exist_ok=True, parents=True)
+                resized_img.save(folder_path / filename)
+
+            return redirect(url_for('relative_caption', dataset=dataset, reference_name=filename))
 
 
 @app.route('/<string:dataset>')
@@ -133,15 +177,22 @@ def compute_fashionIQ_results(caption: str, combiner: Combiner, n_retrieved: int
     """
 
     target_name = ""
-    dress_type = ""
 
-    # Assign the correct FashionIQ category to the reference image
+    # Assign the correct Fashion category to the reference image
     if reference_name in fashionIQ_dress_index_names:
         dress_type = 'dress'
     elif reference_name in fashionIQ_toptee_index_names:
         dress_type = 'toptee'
     elif reference_name in fashionIQ_shirt_index_names:
         dress_type = 'shirt'
+    else:  # Search for an uploaded image
+        for iter_path in app.config['UPLOAD_FOLDER'].rglob('*'):
+            if iter_path.name == reference_name:
+                image_path = iter_path
+                dress_type = image_path.parent.name
+                break
+        else:
+            raise ValueError()
 
     # Check if the query belongs to the validation set and get query info
     for triplet in fashionIQ_val_triplets:
@@ -154,35 +205,39 @@ def compute_fashionIQ_results(caption: str, combiner: Combiner, n_retrieved: int
     if dress_type == "dress":
         if target_name == "":
             index_names = fashionIQ_dress_index_names
-            index_features = fashionIQ_dress_index_features.to(device)
+            index_features = fashionIQ_dress_index_features
         else:
             index_features = fashionIQ_val_dress_index_features.to(device)
             index_names = fashionIQ_val_dress_index_names
-        reference_index = index_names.index(reference_name)
-        index_features = index_features.to(device)
     elif dress_type == "toptee":
         if target_name == "":
             index_names = fashionIQ_toptee_index_names
-            index_features = fashionIQ_toptee_index_features.to(device)
+            index_features = fashionIQ_toptee_index_features
         else:
-            index_features = fashionIQ_val_toptee_index_features.to(device)
+            index_features = fashionIQ_val_toptee_index_features
             index_names = fashionIQ_val_toptee_index_names
-        reference_index = index_names.index(reference_name)
-        index_features = index_features.to(device)
     elif dress_type == "shirt":
         if target_name == "":
             index_names = fashionIQ_shirt_index_names
-            index_features = fashionIQ_shirt_index_features.to(device)
+            index_features = fashionIQ_shirt_index_features
         else:
-            index_features = fashionIQ_val_shirt_index_features.to(device)
+            index_features = fashionIQ_val_shirt_index_features
             index_names = fashionIQ_val_shirt_index_names
-        reference_index = index_names.index(reference_name)
-        index_features = index_features.to(device)
     else:
         raise ValueError()
 
+    index_features = index_features.to(device)
+
     # Get visual features, extract textual features and compute combined features
-    reference_features = index_features[reference_index].unsqueeze(0)
+    try:
+        reference_index = index_names.index(reference_name)
+        reference_features = index_features[reference_index].unsqueeze(0)
+    except Exception:  # raise an exception if the reference image has been uploaded by the user
+        image_path = app.config['UPLOAD_FOLDER'] / 'fashionIQ' / dress_type / reference_name
+        pil_image = PIL.Image.open(image_path).convert('RGB')
+        image = targetpad_transform(1.25, clip_model.visual.input_resolution)(pil_image).to(device)
+        reference_features = clip_model.encode_image(image.unsqueeze(0))
+
     text_inputs = clip.tokenize(caption, truncate=True).to(device)
     with torch.no_grad():
         text_features = clip_model.encode_text(text_inputs)
@@ -223,8 +278,15 @@ def compute_cirr_results(caption: str, combiner: Combiner, n_retrieved: int, ref
 
     # Get visual features, extract textual features and compute combined features
     text_inputs = clip.tokenize(caption, truncate=True).to(device)
-    reference_index = index_names.index(reference_name)
-    reference_features = index_features[reference_index].unsqueeze(0)
+    try:
+        reference_index = index_names.index(reference_name)
+        reference_features = index_features[reference_index].unsqueeze(0)
+    except Exception:  # raise an exception if the reference image has been uploaded by the user
+        image_path = app.config['UPLOAD_FOLDER'] / 'cirr' / reference_name
+        pil_image = PIL.Image.open(image_path).convert('RGB')
+        image = targetpad_transform(1.25, clip_model.visual.input_resolution)(pil_image).to(device)
+        reference_features = clip_model.encode_image(image.unsqueeze(0))
+
     with torch.no_grad():
         text_features = clip_model.encode_text(text_inputs)
         predicted_features = combiner.combine_features(reference_features, text_features).squeeze(0)
@@ -253,7 +315,7 @@ def compute_cirr_results(caption: str, combiner: Combiner, n_retrieved: int, ref
 @app.route('/get_image/<string:image_name>/<string:gt>')
 def get_image(image_name: str, dim: Optional[int] = None, gt: Optional[str] = None):
     """
-    get CIRR or FashionIQ Image
+    get CIRR, FashionIQ or an uploaded Image
     :param image_name: image name
     :param dim: size to resize the image
     :param gt: if 'true' the has a green border, if 'false' has a red border anf if 'none' has a tiny black border
@@ -264,8 +326,13 @@ def get_image(image_name: str, dim: Optional[int] = None, gt: Optional[str] = No
         image_path = server_base_path / 'cirr_dataset' / f'{cirr_name_to_relpath[image_name]}'
     elif image_name in fashion_index_names:
         image_path = server_base_path / 'fashionIQ_dataset' / 'images' / f"{image_name}.jpg"
-    else:
-        raise ValueError()
+    else:  # Search for an uploaded image
+        for iter_path in app.config['UPLOAD_FOLDER'].rglob('*'):
+            if iter_path.name == image_name:
+                image_path = iter_path
+                break
+        else:
+            raise ValueError()
 
     # if 'dim' is not None resize the image
     if dim:
@@ -293,6 +360,10 @@ def _load_assets():
     """
     Load all the necessary assets
     """
+
+    app.config['UPLOAD_FOLDER'].mkdir(exist_ok=True, parents=True)
+    p = Process(target=delete_uploaded_images)
+    p.start()
 
     # load CIRR assets ---------------------------------------------------------------------------------------
     load_cirr_assets()
@@ -430,6 +501,21 @@ def load_cirr_assets():
     global cirr_index_names
     cirr_index_features = torch.vstack((cirr_val_index_features, cirr_test_index_features))
     cirr_index_names = cirr_val_index_names + cirr_test_index_names
+
+
+def delete_uploaded_images():
+    '''
+    For privacy reasons delete the uploaded images after 500 seconds
+    '''
+    FILE_LIFETIME = 500
+    SLEEP_TIME = 50
+    while True:
+        for iter_path in app.config['UPLOAD_FOLDER'].rglob('*'):
+            if iter_path.is_file():
+                if time.time() - iter_path.stat().st_mtime > FILE_LIFETIME:
+                    iter_path.unlink()
+
+        time.sleep(SLEEP_TIME)
 
 
 if __name__ == '__main__':
