fix(*): perform the normalization of the index features at inference time

app.py
@@ -6,7 +6,7 @@ import time
 from io import BytesIO
 from multiprocessing import Process
 from typing import Optional, Tuple, Union
-
+import torch.nn.functional as F
 import PIL.Image
 import PIL.ImageOps
 import clip
@@ -246,6 +246,7 @@ def compute_fashionIQ_results(caption: str, combiner: Combiner, n_retrieved: int
         predicted_features = combiner.combine_features(reference_features, text_features).squeeze(0)
 
     # Sort the results and get the top 50
+    index_features = F.normalize(index_features)
     cos_similarity = index_features @ predicted_features.T
     sorted_indices = torch.topk(cos_similarity, n_retrieved, largest=True).indices.cpu()
     sorted_index_names = np.array(index_names)[sorted_indices].flatten()
@@ -294,6 +295,7 @@ def compute_cirr_results(caption: str, combiner: Combiner, n_retrieved: int, ref
         predicted_features = combiner.combine_features(reference_features, text_features).squeeze(0)
 
     # Sort the results and get the top 50
+    index_features = F.normalize(index_features)
     cos_similarity = index_features @ predicted_features.T
     sorted_indices = torch.topk(cos_similarity, n_retrieved, largest=True).indices.cpu()
     sorted_index_names = np.array(index_names)[sorted_indices].flatten()

extract_features.py
@@ -3,7 +3,6 @@ from typing import Union
 
 import clip
 import torch
-import torch.nn.functional as F
 from torch import nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -43,7 +42,7 @@ def extract_and_save_index_features(dataset: Union[CIRRDataset, FashionIQDataset
         # extract and concatenate features and names
         with torch.no_grad():
             batch_features = clip_model.encode_image(images)
-            index_features = torch.vstack((index_features, F.normalize(batch_features, dim=-1)))
+            index_features = torch.vstack((index_features, batch_features))
             index_names.extend(names)
 
     # save the extracted features

hubconf.py
@@ -5,8 +5,9 @@ from torch import nn
 
 from model import Combiner
 
-CIRR_URL = "https://www.dropbox.com/s/2mjnvlxnk96aedt/cirr_combiner.pth?dl=1"
-FASHIONIQ_URL = "https://www.dropbox.com/s/29xl1gqmrkvbc2n/fashionIQ_combiner.pth?dl=1"
+CIRR_URL = "https://www.dropbox.com/s/cdesqz7yincaq8g/cirr_combiner.pth?dl=1"
+FASHIONIQ_URL = "https://www.dropbox.com/s/tra1no8ionus3lk/fashionIQ_combiner.pth?dl=1"
+
 
 if torch.cuda.is_available():
     torch.cuda.set_device(0)